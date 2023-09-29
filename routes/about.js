a9a7a663cc3499cee879eb229c5679a00a9a5b51
Pushpundu Jana <91383539+pushpendu00@users.noreply.github.com>
2023-09-30 01:12:39 +0530

    Delete routes directory

--- routes/about.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/',require('../controllers/about_controller').about);
-
-
-module.exports = router;
--- routes/register.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/',require('../controllers/register_controller').register);
-
-
-
-
-module.exports = router;
